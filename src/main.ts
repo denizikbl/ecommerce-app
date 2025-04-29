60ceee0d10ebf4af848204df18d4b170244636d3
Deniz İkbal Saylık <ikbalsayliq@gmail.com>
2025-04-30 00:02:10 +0300

    feat: add AllExceptionsFilter for global error handling

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformResponseInterceptor } from './common/interceptors/transform-response.interceptor';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+
 
 
 async function bootstrap() {
@@ -12,7 +14,7 @@ async function bootstrap() {
     transform: true,
   }));
   app.useGlobalInterceptors(new TransformResponseInterceptor());
-
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
