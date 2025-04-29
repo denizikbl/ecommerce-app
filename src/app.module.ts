826a3a1a92ca06e528278538a1e51d7a514f2297
Deniz İkbal Saylık <ikbalsayliq@gmail.com>
2025-04-29 03:19:31 +0300

    feat: create Orders, Payments, and Auth modules structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, OrdersModule, PaymentsModule, AuthModule],
   controllers: [],
   providers: [],
 })
